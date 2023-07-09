f64e5ae4b6df90bd97624d7fe288d558bc26d8a2
5idneyD <sldriscoll@btinternet.com>
2023-07-09 09:43:48 +0100

    Can now add sales invoices - will apply the same to purchases

--- dist/js/addInvoiceRow.js
+++ dist/js/addInvoiceRow.js
@@ -1,18 +1,18 @@
-var row_number=2,table=document.querySelector("#bodyTable"),number_of_rows_input=document.querySelector("#number_of_rows");number_of_rows_input.value=1;function addRow(){var t=`
-        <tr class="invoiceRow">
-            <td>`+row_number+`
-            <td><input type="text" name='`+row_number+`_nominal_code'></td>
-            <td><input type="text" name='`+row_number+`_description'></td>
-            <td><input type="text" name='`+row_number+`_net_value' class='net'  onchange="calculate()"></td>
-            <td><input type="text" name='`+row_number+`_vat' class='vat'></td>
-            <td><input type="text" name='`+row_number+`_total_value' class='tv'></td>
-        </tr>
- `;row_number+=1,table.insertAdjacentHTML("beforeend",t),table.dataset.rows=row_number-1,number_of_rows_input.value=table.dataset.rows}function addBankRecRow(){var t=`
-        <tr>
-            <td>`+row_number+`
-            <td><input type="date" name='`+row_number+`_date'></td>
-            <td><input type="text" name='`+row_number+`_invoice_number'></td>
-            <td><input type="text" name='`+row_number+`_reference' class='net'></td>
-            <td><input type="text" name='`+row_number+`_value' class='vat'></td>
-        </tr>
- `;row_number+=1,table.insertAdjacentHTML("beforeend",t),table.dataset.rows=row_number-1,number_of_rows_input.value=table.dataset.rows}
+var row_number=2,table=document.querySelector(".invoice-body"),number_of_rows_input=document.querySelector("#number_of_rows");number_of_rows_input.value=1;function addRow(){var e=`
+        <div class="invoiceRow">
+            <div>`+row_number+`</div>
+            <div><input type="text" name='`+row_number+`_nominal_code'></div>
+            <div><input type="text" name='`+row_number+`_description'></div>
+            <div><input type="text" name='`+row_number+`_net_value' class='net' onchange="calculate()"></div>
+            <div><input type="text" name='`+row_number+`_vat' class='vat'></div>
+            <div><input type="text" name='`+row_number+`_total_value' class='tv'></div>
+        
+ `;console.log(row_number),row_number+=1,table.insertAdjacentHTML("beforeend",e),table.dataset.rows=row_number-1,number_of_rows_input.value=table.dataset.rows}function addBankRecRow(){var e=`
+        <div>
+            <div>`+row_number+`
+            <div><input type="date" name='`+row_number+`_date'></div>
+            <div><input type="text" name='`+row_number+`_invoice_number'></div>
+            <div><input type="text" name='`+row_number+`_reference' class='net'></div>
+            <div><input type="text" name='`+row_number+`_value' class='vat'></div>
+        </div>
+ `;row_number+=1,table.insertAdjacentHTML("beforeend",e),table.dataset.rows=row_number-1,number_of_rows_input.value=table.dataset.rows}
--- src/js/addInvoiceRow.js
+++ src/js/addInvoiceRow.js
@@ -1,20 +1,21 @@
 var row_number = 2
-var table = document.querySelector("#bodyTable");
+var table = document.querySelector(".invoice-body");
 var number_of_rows_input = document.querySelector("#number_of_rows");
 number_of_rows_input.value = 1;
 
 function addRow() {
 
     var html = `
-        <tr class="invoiceRow">
-            <td>` + row_number + `
-            <td><input type="text" name='` + row_number + `_nominal_code'></td>
-            <td><input type="text" name='` + row_number + `_description'></td>
-            <td><input type="text" name='` + row_number + `_net_value' class='net'  onchange="calculate()"></td>
-            <td><input type="text" name='` + row_number + `_vat' class='vat'></td>
-            <td><input type="text" name='` + row_number + `_total_value' class='tv'></td>
-        </tr>
+        <div class="invoiceRow">
+            <div>` + row_number + `</div>
+            <div><input type="text" name='` + row_number + `_nominal_code'></div>
+            <div><input type="text" name='` + row_number + `_description'></div>
+            <div><input type="text" name='` + row_number + `_net_value' class='net' onchange="calculate()"></div>
+            <div><input type="text" name='` + row_number + `_vat' class='vat'></div>
+            <div><input type="text" name='` + row_number + `_total_value' class='tv'></div>
+        
  `
+ console.log(row_number)
     row_number += 1;
     table.insertAdjacentHTML("beforeend", html);
     table.dataset.rows = row_number - 1;
@@ -24,13 +25,13 @@ function addRow() {
 function addBankRecRow() {
 
     var html = `
-        <tr>
-            <td>` + row_number + `
-            <td><input type="date" name='` + row_number + `_date'></td>
-            <td><input type="text" name='` + row_number + `_invoice_number'></td>
-            <td><input type="text" name='` + row_number + `_reference' class='net'></td>
-            <td><input type="text" name='` + row_number + `_value' class='vat'></td>
-        </tr>
+        <div>
+            <div>` + row_number + `
+            <div><input type="date" name='` + row_number + `_date'></div>
+            <div><input type="text" name='` + row_number + `_invoice_number'></div>
+            <div><input type="text" name='` + row_number + `_reference' class='net'></div>
+            <div><input type="text" name='` + row_number + `_value' class='vat'></div>
+        </div>
  `
     row_number += 1;
     table.insertAdjacentHTML("beforeend", html);
