1af9169a2834fef05bfee1e5b8c66fc285c1e042
5idneyD <sldriscoll@btinternet.com>
2023-08-28 13:08:13 +0100

    Changed csp policy and fadeIn ratio

--- dist/js/fadeIn.js
+++ dist/js/fadeIn.js
@@ -1,1 +1,1 @@
-function fadeIn(o){let e=document.querySelector(o);e.style.opacity=0;const t={root:null,threshold:0,rootMargin:"-15%"};document.addEventListener("scroll",function(){new IntersectionObserver(n=>{n.forEach(r=>{const s=r.intersectionRatio;e.style.opacity=s.toFixed(2)})},t).observe(e)})}
+function fadeIn(o){let e=document.querySelector(o);e.style.opacity=0;const t={root:null,threshold:.1,rootMargin:"-15%"};document.addEventListener("scroll",function(){new IntersectionObserver(n=>{n.forEach(r=>{const s=r.intersectionRatio*1.3;e.style.opacity=s.toFixed(2)})},t).observe(e)})}
--- src/js/fadeIn.js
+++ src/js/fadeIn.js
@@ -8,14 +8,14 @@ function fadeIn(object) {
 
     const observerOptions = {
         root: null,
-        threshold: 0.0,
+        threshold: 0.1,
         rootMargin: "-15%",
     };
 
     document.addEventListener("scroll", function () {
         const observer = new IntersectionObserver((entries) => {
             entries.forEach((entry) => {
-                const opacity = entry.intersectionRatio;
+                const opacity = entry.intersectionRatio * 1.3;
 
                 // Update the opacity of the image
                 obj.style.opacity = opacity.toFixed(2); // Limit to 2 decimal places
